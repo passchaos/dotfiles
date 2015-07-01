9494970b9425642da5341ff2f598254a7d8bf133
Lucas Vieira <lucas_samuel2005@hotmail.com>
2015-07-01 19:40:48 -0300

    Added notification prompts to gamemode.

--- bashscripts/gamemode.sh
+++ bashscripts/gamemode.sh
@@ -11,10 +11,12 @@ if [ "$1" = "on" ]
 then
 	sudo -u lucas pkill compton
 	cpupower frequency-set -g performance
+	notify-send "GAMEMODE: Modo de Jogo Ativo."
 elif [ "$1" = "off" ]
 then
 	cpupower frequency-set -g powersave
 	sudo -u lucas compton &
+	notify-send "GAMEMODE: Modo de Jogo Inativo."
 else
 	echo gamemode: Opção $1 desconhecida.
 fi

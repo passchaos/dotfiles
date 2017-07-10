3db442aefa6a71e949867b1f1f23bfa35102d765
Lucas Vieira <lucas.samuel2002@gmail.com>
2017-07-09 23:41:17 -0300

    General script updates.

--- misc/bashscripts/browser-open-prompt.sh
+++ misc/bashscripts/browser-open-prompt.sh
@@ -2,13 +2,32 @@
 
 PROMPT="URL de Acesso:"
 REGEX='^(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-‌​A-Za-z0-9\+&@#/%=~_|‌​]$'
-COMMON="https://\nhttp://\nDuckDuckGo\nFacebook\nTwitter\nNetflix\nInbox\nYouTube\nGitHub\nHackerNews"
+COMMON="https://\nhttp://\nDuckDuckGo\nFacebook\nTwitter\nNetflix\nInbox\nYouTube\nGitHub\nHackerNews\nInstagram\nWhatsApp"
+
+# If we have a favorite websites file, then run it now
+if [ -f ~/.browser-favorites.sh ] ; then
+    source ~/.browser-favorites.sh
+fi
+
+# If we have a favorite websites file, then read it and append to the hash
+#if [ -f ~/.browser-favs ] ; then
+#    while IFS='' read -r first ; read -r second ; do
+#        PREFS["$first"]="$second" ;
+#    done
+#fi
+
+# Also, loop through our favorites list and append them to our commons list
+for website in "${!PREFS[@]}" ; do
+    COMMON="${COMMON}\n${website}" ;
+done
+
+
 
 # Get access url
 URL=`printf "$COMMON" | dmenu -b -fn '-lucy-tewi-medium-r-normal-*-11-90-*-*-*-*-*-*' -nb '#000000' -nf '#FFFFFF' -sb '#5E637E' -l 0 -p "$PROMPT"`
 
 
-# Switch for interesting options
+# Switch for default options
 case $URL in
     "DuckDuckGo")
         URL="https://duckduckgo.com/" ;;
@@ -26,7 +45,20 @@ case $URL in
         URL="https://github.com/" ;;
     "HackerNews")
         URL="https://news.ycombinator.com/" ;;
+    "Instagram")
+        URL="https://instagram.com/" ;;
+    "WhatsApp")
+        URL="https://web.whatsapp.com/" ;;
 esac
+
+# Switch for user-created options
+# If it is set...
+if [ -z ${PREFS+x} ] ; then
+    #Check if input is in hash table
+    if test "${PREFS[$URL]+isset}" ; then
+        URL="${PREFS[$URL]}"
+    fi
+fi
 
 
 if [ -z "$URL" ] ; then

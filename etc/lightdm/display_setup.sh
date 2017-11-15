99e9ffc800aa82f9eaf03523b226ec1b54cf899d
Lucas Vieira <lucasvieira@protonmail.com>
2017-11-14 22:49:32 -0200

    Changed startup scripts for display.

--- etc/lightdm/display_setup.sh
+++ etc/lightdm/display_setup.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 xrandr --setprovideroutputsource modesetting NVIDIA-0
-xrandr --auto
+if (xrandr | grep "HDMI-0 connected" > /dev/null) then
+    xrandr --output HDMI-0 --mode 0x2bb --output VGA-0 --off --output LVDS-1-1 --off
+    nvidia-settings --assign CurrentMetaMode="HDMI-0: 1920x1080_60i +0+0 {viewportin=1920x1080, viewportout=1840x1015+40+33}"
+else
+    xrandr --output VGA-0 --off --output LVDS-1-1 --auto
+fi
+
